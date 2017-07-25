1c2f9784bdd18bc7667674702a8cf98ecca323d3
Edward King <edwardking@Edwards-MacBook-Pro.local>
2017-07-25 17:30:53 -0600

    Freelancer first commit

---
+++ content.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @package Ryu
+ */
+?>
+<?php if (! is_admin()) : ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'clear' ); ?>>
+  <div class="entry-wrap wrap clear">
+    <?php if ( '' != get_the_post_thumbnail() ) : ?>
+      <?php if (( ! is_single() ) && (! wp_is_mobile()) ): ?>
+      <div class="post_image">
+      <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'ryu' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="<?php the_ID(); ?>" class="ryu-featured-thumbnail">
+        <?php the_post_thumbnail( 'ryu-featured-thumbnail' ); ?>
+      </a></div>
+      <?php else : ?>
+        <?php //the_post_thumbnail( 'ryu-featured-thumbnail' ); ?>
+      <?php endif; ?>
+    <?php endif; ?>
+
+    <div class="entry-header">
+      <?php
+        if ( ! is_single() ) :
+          the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+        else :
+          the_title( '<h1 class="entry-title">', '</h1>' );
+        endif;
+      ?>
+      <div class = "author-bubble">
+          <!--BLABBOS WAS HERE-->
+          <span><?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></span>
+          <span>
+          <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )?>">
+          <?php echo get_the_author(); ?></a>
+          </span>
+          <span>
+          <?php if(function_exists('wp_ulike')) wp_ulike('get'); ?>
+          </span><br>
+          <!--<span>+ -</span>-->
+          <?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+          <span class="comments-link"><?php comments_popup_link( __( 'leave a comment', 'ryu' ), __( '1 Comment', 'ryu' ), __( '% Comments', 'ryu' ) ); ?></span>
+          <?php endif; ?>
+
+          <?php edit_post_link( __( 'edit', 'ryu' ), '<span class="edit-link">', '</span>' ); ?>
+      </div><!--author-bubble-->      
+    </div><!-- .entry-header -->
+    
+      <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+        <div class="entry-summary">
+          <?php the_excerpt(); ?>
+        </div><!-- .entry-summary -->
+      <?php else : ?>
+        <?php if ( '' == get_the_post_thumbnail() ) : ?>    
+            <div style="float: left;">
+        <?php else : ?>
+            <div class="paragraph">
+        <?php endif; ?>        
+          <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'ryu' ) ); ?>
+          <?php
+            wp_link_pages( array(
+              'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'ryu' ) . '</span>',
+              'after'       => '</div>',
+              'link_before' => '<span>',
+              'link_after'  => '</span>',
+            ) );
+          ?>
+        <div>
+      <?php endif; ?>
+  </div><!-- .entry-wrap -->
+</article><!-- #post-## -->
+<?php endif; ?>
--- functions.php
+++ functions.php
@@ -2,36 +2,10 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
 
-function ryu_entry_meta()
-{
-    ?>
-    <!--TODO: move this into HTML / CSS file -->
-    </footer><!--entry-meta-->
-    <footer class = "author-bubble">
-        <!--BLABBOS WAS HERE-->
-        <span><?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></span>
-        <span>
-        <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )?>">
-        <?php echo get_the_author(); ?></a>
-        </span>
-        <span>
-        <?php if(function_exists('wp_ulike')) wp_ulike('get'); ?>
-        </span><br>
-        <!--<span>+ -</span>-->
-        <?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-        <span class="comments-link"><?php comments_popup_link( __( 'leave a comment', 'ryu' ), __( '1 Comment', 'ryu' ), __( '% Comments', 'ryu' ) ); ?></span>
-        <?php endif; ?>
-
-        <?php edit_post_link( __( 'edit', 'ryu' ), '<span class="edit-link">', '</span>' ); ?>
-    </footer><!--author-bubble-->
-    <footer class = "entry-meta">
-    <?php
-}
+get_template_part( 'content', get_post_format());
 add_filter("wp_nav_menu", "my_nav_menu");
-
 function my_nav_menu($content)
 {
     /** Function to display content below the nav-bar. */
@@ -49,5 +23,4 @@ function my_nav_menu($content)
     }
     return $content . $myst;
 }
-
 ?>
